4e1a05f3047485c74fdce427e7a8804a26ab98cf
Parth Saini <56781801+parthsaini08@users.noreply.github.com>
2022-07-12 22:41:41 +0530

    Update spark_price_at_an_instant.py

--- pipeline/Spark/src/use_cases/spark_price_at_an_instant.py
+++ pipeline/Spark/src/use_cases/spark_price_at_an_instant.py
@@ -2,7 +2,6 @@ import spark_consumer as sp_con
 from pyspark.sql.functions import col, from_json
 
 # Class that gives the price at an instant
-
 class PriceAtAnInstant:
     def __init__() -> None:
         pass
